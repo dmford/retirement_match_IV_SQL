305193617ec777494c15aa7777e6656bc96df243
David Ford <david.m.ford@outlook.com>
2026-06-10 10:18:48 -0400

    Load raw CSV files into DuckDB tables

---
+++ sql/01_load_raw_data.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE TABLE employees_raw AS
+SELECT *
+FROM read_csv_auto('data/raw/employees_raw.csv');
+
+CREATE OR REPLACE TABLE employers_raw AS
+SELECT *
+FROM read_csv_auto('data/raw/employers_raw.csv');
+
+CREATE OR REPLACE TABLE retirement_plans_raw AS
+SELECT *
+FROM read_csv_auto('data/raw/retirement_plans_raw.csv');
+
+CREATE OR REPLACE TABLE household_finances_raw AS
+SELECT *
+FROM read_csv_auto('data/raw/household_finances_raw.csv');
+
+CREATE OR REPLACE TABLE earnings_raw AS
+SELECT *
+FROM read_csv_auto('data/raw/earnings_raw.csv');
